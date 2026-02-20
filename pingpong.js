94efc2eb80b7ceccd35e9d2a0710306d2ecfeb53
yza2109 <yzadora.assuncao@escola.pr.gov.br>
2026-02-20 07:49:06 -0300

    Create pingpong.js

---
+++ pingpong.js
@@ -0,0 +1,29 @@
+// Criar Bolinha
+let raio = 7,5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = raio * 2;
+
+// Velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+// Minha Raquete
+let xRaquete = 5;
+let yRaquete = 150;
+
+// Raquete Oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//Placar
+let meusPontos = 0;
+let pontosOponente = 0;
+
+function setup() {
+     createCanvas(600,400);
+}
+
+function draw(){
+  
+}
