3b1664f0c8820f2679bf89280d12a471c69eb906
yza2109 <yzadora.assuncao@escola.pr.gov.br>
2026-02-23 12:15:07 -0300

    update

--- pingpong.js
+++ pingpong.js
@@ -17,13 +17,28 @@ let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 
 //Placar
-let meusPontos = 0;
+let meusPontos = 0; 
 let pontosOponente = 0;
 
-function setup() {
-     createCanvas(600,400);
+function setup(){
+  createCanvas(600.400);
 }
 
 function draw(){
   
+}
+
+function movimento(){
+  circle (xBolinha,yBolinha,diametro);
+  
+     xBolinha += velocidadeXBolinha;
+     yBolinha += velocidadeYBolinha;
+     if(xBolinha + raio > width || xBolinha-raio < 0){
+         velocidadeXBolinha *= -1;
+     }
+     if(yBolinha + raio > height || yBolinha-raio < 0){
+         velocidadeYBolinha *= -1}}
+
+function raquetes(xRaquete,yRaquete){
+    rect(xRaquete,yRaquete,10,90)
 }
