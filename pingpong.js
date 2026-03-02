b15c5c0832172a82122bd7a77d0e839eaadaea9b
yza2109 <yzadora.assuncao@escola.pr.gov.br>
2026-03-02 12:13:51 -0300

    update

--- pingpong.js
+++ pingpong.js
@@ -1,5 +1,5 @@
 // Criar Bolinha
-let raio = 7,5;
+let raio = 7.5;
 let xBolinha = 100;
 let yBolinha = 200;
 let diametro = raio * 2;
@@ -20,11 +20,39 @@ let yRaqueteOponente = 150;
 let meusPontos = 0; 
 let pontosOponente = 0;
 
+//dificuldade
+
+let dificuldade = "Normal";
+let trilha,ponto,raquetada;
+
+function preload(){
+  trilha = loadSound("trilha.mp3")
+  ponto = loadSound("ponto.mp3")
+  raquetada = loadSound("raquetada.mp3")
+}
 function setup(){
+  
   createCanvas(600.400);
 }
 
 function draw(){
+  if(keyIsDown(DOWN_ARROW)){
+    
+  }
+   if(xBolinha -raio < xRaquete + 10 &
+     yBolinha-raio< yRaquete + 90 &
+     yBolinha +raio > yRaquete ){
+    velocidadeXBoinha *= -1;
+    raquete.play()}
+if(xBolinha + raio > xRaqueteOponente &
+   yBolinha - raio < yRaqueteOponente + 90 &
+   yBolinha + raio > yRaqueteOponente) {
+  velocidadeXBolinha *= -1;
+raquetada.play()}
+
+  
+  
+  
   
 }
 
@@ -42,3 +70,5 @@ function movimento(){
 function raquetes(xRaquete,yRaquete){
     rect(xRaquete,yRaquete,10,90)
 }
+
+ 
